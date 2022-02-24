e134224b89f9d828cf6905696492bd4d877c2f37
sheyenne de morais carvalho <sheyenne_22@hotmail.com>
2022-02-24 16:51:14 -0400

    angular - parte 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HomeComponent } from './views/home/home.component';
+import { JogadorComponent } from './views/jogador/jogador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "contato", component: ContatoComponent},
+  {path: "jogador", component: JogadorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
